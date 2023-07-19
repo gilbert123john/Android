c793b4bbb71811e982744a3a7608b8955567cb1c
gilbert123john <johngilbert.kochery@gmail.com>
2023-07-19 10:40:26 +0530

    android code commit

---
+++ ThreeActivity/app/src/main/java/com/example/threeactivity/SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.threeactivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+    public void onMax(View v){
+
+    }
+    public void onMin(View v){
+
+    }
+}
---
+++ ValuePass/app/src/main/java/com/example/valuepass/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.example.valuepass;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Bundle b = getIntent().getExtras();
+        String s = b.getString("name");
+        EditText e = (EditText) findViewById(R.id.t2);
+        e.setText(s);
+    }
+    public void onHide(View v){
+        finish();
+    }
+}
