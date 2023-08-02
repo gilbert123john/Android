45b452263f6db36e12531efdd1c7800a9df5ead7
John Gilbert K T <johngilbert.kochery@gmail.com>
2023-08-02 09:59:59 +0530

    code updated

--- ForwardAndBackwordDataTransferBWActivity/app/src/main/java/com/example/forwardandbackworddatatransferbwactivity/MainActivity.java
+++ ForwardAndBackwordDataTransferBWActivity/app/src/main/java/com/example/forwardandbackworddatatransferbwactivity/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.forwardandbackworddatatransferbwactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void onAction(View v){
-
+//    public void onAction(View v){
+//
+//    }
+    public void onSum(View v) {
+        EditText e1,e2;
+        e1 = (EditText)findViewById(R.id.et1);
+        e2 = (EditText)findViewById(R.id.et2);
+        Intent obj = new Intent("act.rp");
+        obj.putExtra("no1", e1.getText().toString());
+        obj.putExtra("no2", e2.getText().toString());
+        startActivityForResult(obj,1);
+    }
+    @Override
+    protected void onActivityResult(int rq,int rc,Intent data){
+        super.onActivityResult(rq,rc, data);
+        EditText e;
+        e = (EditText)findViewById(R.id.et);
+        e.setText(data.getData().toString());
     }
 }
--- ForwardAndBackwordDataTransferBWActivity/app/src/main/java/com/example/forwardandbackworddatatransferbwactivity/SecondActivity.java
+++ ForwardAndBackwordDataTransferBWActivity/app/src/main/java/com/example/forwardandbackworddatatransferbwactivity/SecondActivity.java
@@ -1,5 +1,7 @@
 package com.example.forwardandbackworddatatransferbwactivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,9 +12,20 @@ public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_main);
+        Bundle bb = getIntent().getExtras();
+        String s1,s2;
+        s1 = bb.getString("no1");
+        s2 = bb.getString("no2");
+        int a,b;
+        a = Integer.parseInt(s1);
+        b = Integer.parseInt(s2);
+        int c = a  +b;
+        Intent res = new Intent();
+        res.setData(Uri.parse("" + c));
+        setResult(RESULT_OK,res);
+        finish();
     }
-    public void onAction(View v){
-
-    }
+//    public void onAction(View v){
+//
+//    }
 }
